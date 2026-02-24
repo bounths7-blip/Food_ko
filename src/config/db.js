61eff7fdeb503442839afcc88a89a1f777226221
balameemd-arch <balameemd@gmail.com>
2026-02-24 14:11:05 +0700

    Add db.js configuration file

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+{
+  "name": "food_ko",
+  "version": "1.0.0",
+  "description": "",
+  "homepage": "https://github.com/bounths7-blip/Food_ko#readme",
+  "bugs": {
+    "url": "https://github.com/bounths7-blip/Food_ko/issues"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/bounths7-blip/Food_ko.git"
+  },
+  "license": "ISC",
+  "author": "",
+  "type": "commonjs",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  }
+}
